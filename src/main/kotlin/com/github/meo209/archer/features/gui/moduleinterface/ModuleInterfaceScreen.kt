bf2ca89525aa065b04d50b3592072fbac6636cc4
meo209 <redslyice@outlook.de>
2024-12-21 01:10:49 +0100

    chore: bump version

--- src/main/kotlin/com/github/meo209/archer/features/gui/moduleinterface/ModuleInterfaceScreen.kt
+++ src/main/kotlin/com/github/meo209/archer/features/gui/moduleinterface/ModuleInterfaceScreen.kt
@@ -1,6 +1,15 @@
 package com.github.meo209.archer.features.gui.moduleinterface
 
+import net.minecraft.client.gui.DrawContext
 import net.minecraft.client.gui.screen.Screen
+import net.minecraft.text.Text
 
-class ModuleInterfaceScreen: Screen {
+class ModuleInterfaceScreen: Screen(Text.literal("MI")) {
+
+    override fun render(context: DrawContext, mouseX: Int, mouseY: Int, delta: Float) {
+        super.render(context, mouseX, mouseY, delta)
+
+
+    }
+
 }
