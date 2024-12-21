0ede8f46d4cf581741f7df798c10c7f3da11d220
meo209 <redslyice@outlook.de>
2024-12-21 01:33:07 +0100

    feat: Basic clickgui

---
+++ src/main/java/com/github/meo209/archer/mixin/KeyboardMixin.java
@@ -0,0 +1,24 @@
+package com.github.meo209.archer.mixin;
+
+import com.github.meo209.archer.features.gui.moduleinterface.ModuleInterfaceScreen;
+import net.minecraft.client.Keyboard;
+import net.minecraft.client.MinecraftClient;
+import org.lwjgl.glfw.GLFW;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Keyboard.class)
+public class KeyboardMixin {
+
+    @Inject(method = "onKey", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/Window;getHandle()J", ordinal = 0))
+    public void onKey(long window, int key, int scancode, int action, int modifiers, CallbackInfo ci) {
+        if (action == GLFW.GLFW_PRESS) {
+            if (key == GLFW.GLFW_KEY_RIGHT_SHIFT) {
+                MinecraftClient.getInstance().setScreen(new ModuleInterfaceScreen());
+            }
+        }
+    }
+
+}
