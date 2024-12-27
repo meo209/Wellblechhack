c5555bc6a104ce9ccac296cdb3b47f325fa052f1
meo209 <redslyice@outlook.de>
2024-12-27 16:31:31 +0100

    fix: Range requires value to be in min..max

--- src/main/kotlin/com/github/meo209/archer/features/module/settings/Range.kt
+++ src/main/kotlin/com/github/meo209/archer/features/module/settings/Range.kt
@@ -4,4 +4,10 @@ data class Range(
     var value: Float,
     val min: Float,
     val max: Float
-)
+) {
+    
+    init {
+        require(value in min..max) { "Value needs to be in range of min..max" }
+    }
+    
+}
