9f4de78c8d96d99374657119df598f15cd357a9b
meo209 <redslyice@outlook.de>
2024-12-28 17:11:12 +0100

    docs: Copyright

--- src/main/kotlin/com/github/meo209/archer/utils/math/RotationUtil.kt
+++ src/main/kotlin/com/github/meo209/archer/utils/math/RotationUtil.kt
@@ -1,3 +1,16 @@
+/*
+ * This file is part of Archer (https://github.com/meo209/Archer)
+ * Copyright (c) 2024 - meo209
+ *
+ * Archer is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * Archer program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package com.github.meo209.archer.utils.math
 
 import net.minecraft.client.MinecraftClient
